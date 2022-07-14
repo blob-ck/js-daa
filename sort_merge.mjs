c98e3b95e363a0f77c070b4667611016552a3139
Gwan <hankwan777>
2022-07-14 21:00:14 +0900

    병합정렬 구현(강의와 다르게)

---
+++ sort_merge.mjs
@@ -0,0 +1,36 @@
+// 강의와 다르게 구현 -> 강의 소스와 빅오 비교 필요
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const midIndex = parseInt(arr.length / 2);
+    const firstArr = mergeSort(arr.slice(0, midIndex));
+    const secondArr = mergeSort(arr.slice(midIndex));
+    return sortAtMerge(firstArr, secondArr);
+}
+
+function sortAtMerge(firstArr, secondArr) {
+    let firstIndex = 0;
+    let secondIndex = 0;
+    const sortedArr = [];
+    while(sortedArr.length <= firstArr.length + secondArr.length) {
+        if (firstArr[firstIndex] < secondArr[secondIndex]) {
+            sortedArr.push(firstArr[firstIndex]);
+            firstIndex++;
+            if (!firstArr[firstIndex]) {
+                secondArr.slice(secondIndex).forEach(e => sortedArr.push(e));
+                break;
+            }
+        } else {
+            sortedArr.push(secondArr[secondIndex]);
+            secondIndex++;
+            if (!secondArr[secondIndex]) {
+                firstArr.slice(firstIndex).forEach(e => sortedArr.push(e));
+                break;
+            }
+        }
+    }
+    return sortedArr;
+}
+
+console.log(`[ ${mergeSort([3,6,1,65,7,467,5,1,54,234]).join(', ')} ]`);
