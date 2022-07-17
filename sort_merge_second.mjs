cc4c83ff238304220db49d2b91a11f78e428478f
hankw <hankwan777@gmail.com>
2022-07-17 16:21:13 +0900

    병합정렬 구현 에러 수정

--- sort_merge_second.mjs
+++ sort_merge_second.mjs
@@ -3,39 +3,41 @@ function mergeSort(arr, leftIndex, rightIndex) {
         const midIndex = parseInt((leftIndex + rightIndex) / 2);
         mergeSort(arr, leftIndex, midIndex);
         mergeSort(arr, midIndex + 1, rightIndex);
-        merge(arr, midIndex, leftIndex, rightIndex);
+        merge(arr, leftIndex, midIndex, rightIndex);
     }
 }
 
-function merge(arr, midIndex, leftIndex, rightIndex) {
-    console.log(arr, midIndex, leftIndex, rightIndex);
+function merge(arr, leftIndex, midIndex, rightIndex) {
     if (arr.length <= 1) return;
+
     const tempArr = [];
     const startIndex = leftIndex;
-    const length = rightIndex - midIndex + 1;
+    const length = rightIndex - leftIndex + 1; // for문 반복횟수를 이렇게 애매하게 할 바엔 while을 쓰는게 나아보임
+
+    let leftArrIndex = leftIndex;
+    let rightArrIndex = midIndex + 1;
     let currentNumber;
+    
     for (let i = 0; i < length; i++) {
-        if (leftIndex > midIndex) {
-            arr.slice(midIndex + 1).forEach(n => tempArr.push(n));
+        if (leftArrIndex > midIndex) {
+            arr.slice(rightArrIndex, rightIndex + 1).forEach(n => tempArr.push(n));
             break;
         }
-        if (rightIndex < midIndex) {
-            arr.slice(leftIndex, midIndex + 1).forEach(n => tempArr.push(n));
+        if (rightArrIndex > rightIndex) {
+            arr.slice(leftArrIndex, midIndex + 1).forEach(n => tempArr.push(n));
             break;
         }
 
-        currentNumber = (arr[leftIndex] < arr[rightIndex]) 
-                            ? arr[leftIndex++]
-                            : arr[rightIndex++];
-
+        currentNumber = (arr[leftArrIndex] < arr[rightArrIndex]) 
+                            ? arr[leftArrIndex++]
+                            : arr[rightArrIndex++];
+                            console.log(currentNumber);
         tempArr.push(currentNumber);
     }
     
-    console.log(tempArr);
     tempArr.forEach((n, i) => arr[startIndex + i] = n);
 }
 
-// const arr = [ 3, 6, 1, 65, 7, 467, 5, 1, 54, 234 ];
-const arr = [ 3, 6, 1, 65 ];
+const arr = [ 3, 6, 1, 65, 7, 467, 5, 1, 54, 234 ];
 mergeSort(arr, 0, arr.length - 1);
 console.log(`[ ${arr.join(', ')} ]`);
