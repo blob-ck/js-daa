a829b073cc97ac2404a68cd145d5167286ebb291
Gwan <hankwan777>
2022-07-20 17:44:59 +0900

    상향식 피보나치 및 출력함수 추가

--- fibonacci.mjs
+++ fibonacci.mjs
@@ -1,15 +1,23 @@
-let num = 40;
+let num = 10;
 
-function fibonacci(n) {
-    if (n === 0 || n === 1) return n;
-    return fibonacci(n-2) + fibonacci(n-1);
+function checkTimeForFibonacci(n, func) {
+    console.log('==================================================')
+    const txt = `Check time for executing function ${func.name}`;
+    console.time(txt);
+    console.log(func(n));
+    console.timeEnd(txt);
+    console.log('==================================================')
 }
 
-const fibonacciTime = 'Check time for executing function fibonacci';
-console.time(fibonacciTime);
-fibonacci(num);
-console.timeEnd(fibonacciTime);
+// // 일반 방식
+// function fibonacci(n) {
+//     if (n === 0 || n === 1) return n;
+//     return fibonacci(n-2) + fibonacci(n-1);
+// }
 
+// checkTimeForFibonacci(num, fibonacci);
+
+// 메모이제이션1 방식
 function fibonacciWithMemo(n, memo = {}) {
     if (n === 0 || n === 1) return n;
     memo[n-2] = (memo[n-2] || fibonacciWithMemo(n-2, memo));
@@ -19,11 +27,10 @@ function fibonacciWithMemo(n, memo = {}) {
     return memo[n];
 }
 
-const fibonacciWithMemoTime = 'Check time for executing function fibonacciWithMemo';
-console.time(fibonacciWithMemoTime);
-fibonacciWithMemo(num);
-console.timeEnd(fibonacciWithMemoTime);
+checkTimeForFibonacci(num, fibonacciWithMemo);
 
+
+// 메모이제이션2 방식
 function fibonacciWithMemo2(n, memo = {}) {
     if (n === 0 || n === 1) return n;
     memo[n] = memo[n] || fibonacciWithMemo2(n-2, memo) + fibonacciWithMemo2(n-1, memo);
@@ -31,11 +38,7 @@ function fibonacciWithMemo2(n, memo = {}) {
     return memo[n];
 }
 
-const fibonacciWithMemo2Time = 'Check time for executing function fibonacciWithMemo2';
-console.time(fibonacciWithMemo2Time);
-fibonacciWithMemo2(num);
-console.timeEnd(fibonacciWithMemo2Time);
-
+checkTimeForFibonacci(num, fibonacciWithMemo2);
 
 
 /**
@@ -43,3 +46,19 @@ console.timeEnd(fibonacciWithMemo2Time);
  * Check time for executing function fibonacciWithMemo: 0.152099609375 ms
  * Check time for executing function fibonacciWithMemo2: 0.0498046875 ms
  */
+
+
+
+// 상향식
+function fibonacciWithTabulation(n) {
+    if (n <= 1) return n;
+
+    let table = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        table[i] = table[i-2] + table[i-1];
+    }
+
+    return table[n];
+}
+
+checkTimeForFibonacci(num, fibonacciWithTabulation);
